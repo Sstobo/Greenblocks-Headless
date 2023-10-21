cbcb47214e1c776075e6c48100d4fb70b1462e57
Sstobo <seanstobo@gmail.com>
2023-10-21 12:59:32 -0700

    homepage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 export default async function Page() {
   // get current url, query THAT page in the db, return here
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 text-green-50  ">
       <div className="flex flex-row flex-wrap  gap-4">
-        <div className="border border-green-100 p-4">
-          <h1 className="text-3xl font-bold">Next.js</h1>
-          <p className="text-base">The React Framework for Production</p>
+        <div className="border border-green-100 p-4 hover:scale-105 duration-200 hover:shadow-lg cursor-pointer">
+          <h1 className="text-3xl font-bold ">Sean Stobo</h1>
+          <p className="text-base">A Vancouver based web developer</p>
         </div>
-        <div className="border border-green-100 p-4">
-          <h1 className="text-3xl font-bold">Next.js</h1>
-          <p className="text-base">The React Framework for Production</p>
+        <div className="border border-green-100 p-4 hover:scale-105 duration-200 hover:shadow-lg cursor-pointer">
+          <h1 className="text-3xl  font-bold">Next.js</h1>
+          <p className="text-base">The React Framework for Full Stack Production</p>
         </div>
-        <div className="border border-green-100 p-4">
-          <h1 className="text-3xl font-bold">Next.js</h1>
-          <p className="text-base">The React Framework for Production</p>
+        <div className="border border-green-100 p-4 hover:scale-105 duration-200 hover:shadow-lg cursor-pointer">
+          <h1 className="text-3xl font-bold">Wordpress</h1>
+          <p className="text-base">The most popular Content Management System on the planet</p>
         </div>
       </div>
     </main>
--- src/components/LeftBar/LeftBar.tsx
+++ src/components/LeftBar/LeftBar.tsx
@@ -4,22 +4,28 @@ import getAllPosts from "@/lib/getAllPosts";
 
 export default async function LeftBar() {
   const allPosts = await getAllPosts();
-
+ console.log(allPosts)
   return (
     <div className="flex flex-col p-3">
       <h2 className="text-lg text-green-50 ">Recent Posts:</h2>
       {allPosts &&
         allPosts?.map((post: any) => (
-          <div key={post.slug} className="text-lg">
+          <div key={post.slug}>
+            
             <a
               href={`/posts/${post.slug}`}
               className="group text-xs text-green-50 transition-all duration-300 ease-in-out"
             >
-              <span className="bg-left-bottom bg-gradient-to-r from-green-50 to-green-50 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_1px] transition-all duration-500 ease-out">
+              <span className="bg-left-bottom bg-gradient-to-r from-green-50 to-green-50 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_1px] transition-all duration-500 ease-out ">
                 {post.title}
+               
               </span>
             </a>
+            <div className="text-xs text-green-50 opacity-70 pt-2">
+            Written on: {post.date}
+            </div>
           </div>
+
         ))}
     </div>
   );
